f44acc589ce5e287cb8d875fbf8e3f08000576fe
soto0113 <83653740+soto0113@users.noreply.github.com>
2023-09-11 18:21:17 -0500

    -

--- followPractProject/followPractApp/urls.py
+++
@@ -1,7 +0,0 @@
-from followPractApp import views 
-from django.urls import path
-urlpatterns = [ 
-    path('estudiantes/', views.estudiantes_list, name='listado_estudiantes'),
-
-    path('estudiantes/<int:estudiante_id>/', views.estudiante_detail, name='detalle_estudiante'),
-]
